37f8f462106bdaac0d55ec3bfb22c2ab31d26aec
Aurelien CAZUC <acazuc@student.42.fr>
2016-03-16 11:14:26 +0100

    Changed error_quit messages to red

--- srcs/error_quit.c
+++ srcs/error_quit.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/29 19:32:12 by acazuc            #+#    #+#             */
-/*   Updated: 2016/03/03 11:45:52 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/03/16 11:12:06 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	error_quit(char *error_message, char *file, int line)
 {
+	ft_putstr_fd("\033[1;31m", 2);
 	ft_putstr_fd(error_message, 2);
 	ft_putstr_fd(" (", 2);
 	ft_putstr_fd(file, 2);
