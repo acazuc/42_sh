cdfe8440f9cc08d7e8ed312a2c7b7133674aa219
Aurelien CAZUC <acazuc@student.42.fr>
2016-09-27 16:12:22 +0200

    Added -n on echo

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 17:15:46 by acazuc            #+#    #+#             */
-/*   Updated: 2016/09/26 19:22:37 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/09/27 16:11:37 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,26 @@
 int		builtin_echo(t_env *env, char **datas, int len)
 {
 	size_t	i;
+	int		not;
 
+	not = 0;
 	i = 1;
-	if (!datas[1])
+	if (len < 2)
 		ft_putchar('\n');
+	if (!ft_strcmp(datas[1], "-n"))
+	{
+		not = 1;
+		++i;
+	}
 	while (datas[i])
 	{
 		ft_putstr(datas[i]);
 		if (datas[i + 1])
 			ft_putchar(' ');
-		else
+		else if (!not)
 			ft_putchar('\n');
 		i++;
 	}
 	(void)env;
-	(void)len;
 	return (1);
 }
